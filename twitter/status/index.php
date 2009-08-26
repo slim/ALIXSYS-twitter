43f573df04e48fcda7c64eb636a499483330a318
Slim Amamou <slim.amamou@gmail.com>
2009-08-26 20:44:33 +0200

    Maximum size of tweet is 140 char

--- twitter/status/index.php
+++ twitter/status/index.php
@@ -14,7 +14,7 @@
 <form action="../update/">
 <input name="k" type="hidden" value="<?php print $_GET['k'] ?>"/>
 <input name="s" type="hidden" value="<?php print $_GET['s'] ?>"/>
-<input name="status" type="text" value="<?php print $status ?>"/>
+<input name="status" type="text" maxlength="140" value="<?php print $status ?>"/>
 <input type="submit" value="update" />
 &nbsp;&nbsp;<a href="<?php print $myfriends ?>">Friends</a>
 <p>tip: bookmark this page</p>
