4c8c11e93cc327ed9a94508206933e729bc300e7
Slim Amamou <slim.amamou@gmail.com>
2010-07-25 01:39:18 +0200

    added a little description

--- twitter/get-api.php
+++ twitter/get-api.php
@@ -9,6 +9,7 @@
 <center style="margin-bottom: 100px">
 <a href="http://alixsys.com"><img src="../png/AliXsys-identite.png" border="0" /></a>
 <p id="description">I am a simple Twitter API OAuth proxy.</p>
+<p>On August 16, 2010, the Twitter will be <a href="http://countdowntooauth.com">shutting off basic authentication on the Twitter API</a>. ALIXSYS twitter API proxy is a dropin replacement solution for your scripts. Easy. </p>
 <a id="register" href="register/">Authenticate with Twitter to obtain your API code</a>
 </center>
 <p id="signature">by <a href="http://alixsys.com">ALIXSYS</a></p>
