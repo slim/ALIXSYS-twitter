60579874c2aac114565dcc493ac9d1ea941c2ee1
Slim Amamou <slim.amamou@gmail.com>
2010-07-25 00:58:55 +0200

    /api/ oauth proxy

--- twitter/api/index.php
+++ twitter/api/index.php
@@ -5,5 +5,14 @@
 	Tweet::$db = new PDO($conf['db']);
 	Tweet::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	Tweet::oauth($conf['consumer_key'], $conf['consumer_secret']);
+	$address = $_GET['axa'];
 
-	Tweet::$twitter->OAuthRequest("https://$url", $update, 'POST');
+
+	if ($_POST) {
+		print Tweet::$twitter->OAuthRequest("https://$address", $_POST, 'POST');
+	}
+	else {
+		unset($_GET['axk']);
+		unset($_GET['axa']);
+		print Tweet::$twitter->OAuthRequest("https://$address", $_GET, 'GET');
+	}
