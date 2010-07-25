1c01c0a9d347cba08fc64c1f8dd0153d61bec7da
Slim Amamou <slim.amamou@gmail.com>
2010-07-25 19:40:15 +0200

    added a link to "get a feeling of the API"

--- twitter/get-api.php
+++ twitter/get-api.php
@@ -9,7 +9,7 @@
 <center style="margin-bottom: 100px">
 <a href="http://alixsys.com"><img src="../png/AliXsys-identite.png" border="0" /></a>
 <p id="description">I am a simple Twitter API OAuth proxy.</p>
-<p>On August 16, 2010, the Twitter will be <a href="http://countdowntooauth.com">shutting off basic authentication on the Twitter API</a>. ALIXSYS twitter API proxy is a dropin replacement solution for your scripts. Easy. </p>
+<p>On August 16, 2010, the Twitter will be <a href="http://countdowntooauth.com">shutting off basic authentication on the Twitter API</a>. ALIXSYS twitter API proxy is a dropin replacement solution for your scripts. You authenticate, then you get a custom key that enables your custom API. Easy. Get a feeling of <a href="doc/api.php?axk=123456789">what your API will look like</a></p>
 <a id="register" href="register/">Authenticate with Twitter to obtain your API code</a>
 </center>
 <p id="signature">by <a href="http://alixsys.com">ALIXSYS</a></p>
